b5f964c56b115f7dc78616bbf3d54edd51d835a3
iroha0611 <munkhod0611@gmail.com>
2016-01-19 11:07:24 +0800

    shirogane

---
+++ app/models/page.rb
@@ -0,0 +1,4 @@
+class Page < ActiveRecord::Base
+	has_attached_file :avatar, styles: { medium: "500x500>", thumb: "300x300>" }, default_url: "/images/:style/missing.png" 
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+end
